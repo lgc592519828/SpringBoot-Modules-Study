73ee473dc39c18d3ce8a48ee1a54542785ac233f
gcheng.L <592519828@qq.com>
2019-09-09 17:39:35 +0800

    springBoot 与 MVC
    Signed-off-by: gcheng.L <592519828@qq.com>

---
+++ springboot-Day5/src/main/java/cn/gcheng/springboot/SpringbootApplication.java
@@ -0,0 +1,16 @@
+package cn.gcheng.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * springBoot 与 MVC
+ */
+@SpringBootApplication
+public class SpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApplication.class, args);
+    }
+
+}
