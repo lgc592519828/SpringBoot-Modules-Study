c4437175d2e9d603f0b249f29328083a4d0ef779
gcheng.L <592519828@qq.com>
2019-09-25 16:32:47 +0800

    统一了springboot 为2.1.8版本
    Signed-off-by: gcheng.L <592519828@qq.com>

---
+++ springboot-Day7/src/main/java/cn/gcheng/springboot/controller/WebController.java
@@ -0,0 +1,15 @@
+package cn.gcheng.springboot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebController {
+
+
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String index() {
+        return "Hello World!";
+    }
+}
