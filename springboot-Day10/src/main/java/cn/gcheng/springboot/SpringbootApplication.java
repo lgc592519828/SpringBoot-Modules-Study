59735e78090b171ac72b9889dcf78db66f460042
gcheng.L <592519828@qq.com>
2019-10-09 19:15:40 +0800

    springboot 2.0 基于配置文件整合 druid
    Signed-off-by: gcheng.L <592519828@qq.com>

---
+++ springboot-Day10/src/main/java/cn/gcheng/springboot/SpringbootApplication.java
@@ -0,0 +1,15 @@
+package cn.gcheng.springboot;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("cn.gcheng.springboot.mapper")
+public class SpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApplication.class, args);
+    }
+
+}
---
+++ springboot-Day10/src/main/java/cn/gcheng/springboot/service/EmpService.java
@@ -0,0 +1,53 @@
+package cn.gcheng.springboot.service;
+
+import cn.gcheng.springboot.entity.Emp;
+import cn.gcheng.springboot.mapper.EmpMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author gcheng.L
+ * @create 2019-10-08 16:44
+ */
+@Service
+public class EmpService {
+
+    @Autowired
+    private EmpMapper empMapper;
+
+    public Emp findById(Integer empno) {
+        return empMapper.findById(empno);
+    }
+
+    public Emp findByAutoId(Integer empno) {
+        return empMapper.findByAutoId(empno);
+    }
+
+    public List<Map> findDepts(String dname, Float sal) {
+        Map param = new HashMap(16);
+        param.put("dname", dname);
+        param.put("sal", sal);
+        return empMapper.findDepts(param);
+    }
+
+
+    @Transactional(rollbackFor = Exception.class)
+    public void save(Emp emp) {
+        empMapper.save(emp);
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    public void update(Emp emp) {
+        empMapper.update(emp);
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    public void delete(Integer empno) {
+        empMapper.delete(empno);
+    }
+}
