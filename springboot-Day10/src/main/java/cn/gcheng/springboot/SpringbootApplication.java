39e88b34b9e3e9eff4f6fba4b73c6219bef429c3
gcheng.L <592519828@qq.com>
2019-10-10 17:23:17 +0800

    添加springbootApplicatgion启动类 注释
    Signed-off-by: gcheng.L <592519828@qq.com>

--- springboot-Day10/src/main/java/cn/gcheng/springboot/SpringbootApplication.java
+++ springboot-Day10/src/main/java/cn/gcheng/springboot/SpringbootApplication.java
@@ -4,6 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * springBoot 整合 Druid 单数据源，配置相关监控
+ */
 @SpringBootApplication
 @MapperScan("cn.gcheng.springboot.mapper")
 public class SpringbootApplication {
