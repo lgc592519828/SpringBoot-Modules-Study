8a8cfb4bb0b5b70760e3ee64785c5906387670d6
gcheng.L <592519828@qq.com>
2019-10-11 16:28:23 +0800

    集成Druid 配置单数据源以及监控，druid相关配置，基于官方提供的starter依赖简化配置
    Signed-off-by: gcheng.L <592519828@qq.com>

--- springboot-Day10/src/main/java/cn/gcheng/springboot/SpringbootApplication.java
+++ springboot-Day10/src/main/java/cn/gcheng/springboot/SpringbootApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * springBoot 整合 Druid 单数据源，配置相关监控
+ * @author GchengLiu
  */
 @SpringBootApplication
 @MapperScan("cn.gcheng.springboot.mapper")
