e59dc529b4293834d9353482d5f06a5f4f55202e
gcheng.L <592519828@qq.com>
2019-10-11 16:17:49 +0800

    filter/Interceptor 过滤器拦截器的配置、原理剖析、工作流程
    Signed-off-by: gcheng.L <592519828@qq.com>

--- springboot-Day6/src/main/java/cn/gcheng/springboot/SpringbootApplication.java
+++ springboot-Day6/src/main/java/cn/gcheng/springboot/SpringbootApplication.java
@@ -3,6 +3,10 @@ package cn.gcheng.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Filter , Interceptor 配置以及原理解析
+ * @author GchengLiu
+ */
 @SpringBootApplication
 public class SpringbootApplication {
 
